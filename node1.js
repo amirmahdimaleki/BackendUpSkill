254a34a31588cfcf4adadc28189ff408a5f73b2d
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-07-25 20:21:58 +0430

    138377-correctFile-2

--- node1.js
+++ node1.js
@@ -5,3 +5,10 @@ setTimeout(()=>{
 },0)
 console.log("last")
 console.timeEnd()
+console.table({
+    name: "AmirMahdi",
+    last_name: "Richards",
+    age: 37.9,
+    isPerfectlyHandSome: true,
+    describeLastParameter: "just kidd'n :)))"
+})
