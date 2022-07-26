7d6ecc876ddc60bf1c0e0cc11188e9b3f2a4c699
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-07-26 22:12:10 +0430

    138377-(event)-1

--- node1.js
+++ node1.js
@@ -1,16 +1,53 @@
-console.time()
-console.log("first")
-setTimeout(()=>{
-    console.log("second")
-},0)
+// console.time()
+// console.log("first")
+// setTimeout(()=>{
+//     console.log("second")
+// },0)
 
 
-console.log("last")
-console.timeEnd()
-console.table({
-    name: "AmirMahdi",
-    last_name: "Richards",
-    age: 37.9,
-    isPerfectlyHandSome: true,
-    describeLastParameter: "just kidd'n :)))"
-})
+// console.log("last")
+// console.timeEnd()
+// console.table({
+//     name: "AmirMahdi",
+//     last_name: "Richards",
+//     age: 37.9,
+//     isPerfectlyHandSome: true,
+//     describeLastParameter: "just kidd'n :)))"
+// })
+
+// * ------------------------------------------------------------------
+//! Async
+
+// const {readFile, writeFile} = require("fs").promises
+// // const util = require('util')
+
+// // const readFilePromise = util.promisify(readFile)
+// // const writeFilePromise = util.promisify(writeFile)
+
+// const fn = async () => {
+//     try {
+//         const file = await readFile('./README.md', 'utf-8')
+//         console.log(file)
+        
+//     } catch (err) {
+//         console.log(err)
+//     }
+// }
+
+// fn()
+
+//  * -----------------------------------------------------------------
+// ! event and emitter
+
+
+const EventEmitter = require('events')
+
+const myEmitter = new EventEmitter()
+
+ myEmitter.on('Birth', ()=>{
+    console.table({
+        hello:"world"
+    })
+ })
+
+myEmitter.emit('Birth')
